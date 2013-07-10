7167444069bb973bb477657355ba92af9d36c0e7
J. Austin Hughey <jaustinhughey@me.com>
2013-07-10 15:38:19 -0500

    more changes from 1.8.7

--- app/models/course_date.rb
+++ app/models/course_date.rb
@@ -11,8 +11,8 @@ class CourseDate < ActiveRecord::Base
   
   before_create :set_sku
   scope :current, where("start_date > ?", Date.today.strftime('%Y-%m-%d')) 
-  scope :available, where("class_size >= ?", OrderItem.where(:course_date_id=>self.id).count) 
-  scope :waiting, where("class_size < ?", OrderItem.where(:course_date_id=>self.id).count) 
+  scope :available, where("class_size >= ?", OrderItem.where(:course_date_id => id).count) 
+  scope :waiting, where("class_size < ?", OrderItem.where(:course_date_id => id).count) 
     
   scope :current_and_recent, where("start_date > ?", Date.today.months_ago(1).strftime('%Y-%m-%d'))
   
